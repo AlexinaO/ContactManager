0f8d4a2acd37b0391330ab8efbb8b2ad229067c1
Vincent <vincent120@orange.fr>
2018-05-30 16:25:31 +0200

    Ajout Contact

---
+++ ContactManager/Metier/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactsManager.Metier
+{
+    public class Contact
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+    }
+}
